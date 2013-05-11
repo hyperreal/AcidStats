b021147146d8049794dfd7a4f529d8c1876ad82e
fajka <fajka@hyperreal.info>
2013-05-11 23:53:21 +0200

    embedded jetty

--- src/main/java/info/hyperreal/acid/stats/domain/repositories/StatRowRepositoryImpl.java
+++ src/main/java/info/hyperreal/acid/stats/domain/repositories/StatRowRepositoryImpl.java
@@ -1,10 +1,10 @@
 package info.hyperreal.acid.stats.domain.repositories;
 
-import com.google.inject.Singleton;
 import info.hyperreal.acid.stats.domain.Banner;
 import info.hyperreal.acid.stats.domain.StatRow;
 import info.hyperreal.acid.stats.exceptions.BannerNotFoundException;
 
+import javax.inject.Singleton;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -12,7 +12,7 @@ import java.util.concurrent.ConcurrentHashMap;
 @Singleton
 public class StatRowRepositoryImpl implements StatRowRepository {
 
-    private Map<Integer, List<StatRow>> rows = new ConcurrentHashMap<Integer, List<StatRow>>();
+    private Map<Integer, List<StatRow>> rows = new ConcurrentHashMap<>();
 
     @Override
     public List<StatRow> findRowsByBanner(Banner banner) throws BannerNotFoundException {
